282e2459d2399a251ac438b215ffd4230892d32e
alanhermawan33-sys <alanhermawan33@gmail.com>
2026-08-10 19:52:51 +0700

    Update supabaseClient.ts

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://sumywlwhlwcczhxclijt.supabase.co';
-const supabaseAnonKey = 'PASTE_PUBLISHABLE_ANON_KEY_KAMU_DI_SINI';
+const supabaseAnonKey = 'sb_publishable_CgDaMApcv9067qXNx7w7Og_rKS32gMV';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
