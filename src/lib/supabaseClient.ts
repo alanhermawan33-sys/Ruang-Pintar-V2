4dc1975abe179f5c5ebe7d54b3fef0bec1022f0f
alanhermawan33-sys <alanhermawan33@gmail.com>
2026-08-10 19:45:45 +0700

    Update supabaseClient.ts

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-// Menggunakan fallback dummy jika ENV belum terbaca Vercel agar aplikasi tidak crash (blank)
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://xyzcompany.supabase.co';
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'dummy-key';
+const supabaseUrl = 'https://sumywlwhlwcczhxclijt.supabase.co';
+const supabaseAnonKey = 'PASTE_PUBLISHABLE_ANON_KEY_KAMU_DI_SINI';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
